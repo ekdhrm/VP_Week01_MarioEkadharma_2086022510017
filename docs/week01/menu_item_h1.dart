e6db6bfb24fdc12ef860d1bb0039e87230dd9e94
ekdhrm <mekadhrm@student.ciputra.ac.id>
2026-09-24 13:06:03 +0800

    finished homework-1 : updated to docs/week01

---
+++ docs/week01/menu_item_h1.dart
@@ -0,0 +1,37 @@
+class MenuItem {
+  final String name;
+  final double price;
+  final double? discountPercent;
+
+  MenuItem({required this.name, required this.price, this.discountPercent});
+
+  double finalPrice() {
+    if (discountPercent != null) {
+      return price - (price * discountPercent! / 100);
+    }
+    return price;
+  }
+}
+
+void main() {
+  List<MenuItem> menuItems = [
+    MenuItem(name: 'Nasi Goreng', price: 15000, discountPercent: 10),
+    MenuItem(name: 'Mie Ayam', price: 12000),
+    MenuItem(name: 'Sate Ayam', price: 20000, discountPercent: 5),
+  ];
+
+  List<String> allItems = menuItems.map((item) => item.name).toList();
+  // ignore: avoid_print
+  allItems.forEach(print);
+
+  List<MenuItem> itemsUnder15000 = menuItems
+      .where((item) => item.finalPrice() < 15000)
+      .toList();
+  // ignore: avoid_function_literals_in_foreach_calls, avoid_print
+  itemsUnder15000.forEach((item) => print('${item.name}: ${item.finalPrice()}'),
+  );
+
+  double totalPrice = menuItems.fold(0, (sum, item) => sum + item.finalPrice());
+  // ignore: avoid_print
+  print('Total Price: $totalPrice');
+}
